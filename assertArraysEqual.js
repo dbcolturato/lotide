cd75fcd20ce5a36db090eecb7b6ca011e5e706c3
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-09-15 18:22:11 +0000

    Implement assertArraysEqual Lotide Problem

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArray = function(array1, array2) {
+  return Array.isArray(array1)
+    && Array.isArray(array2)
+    && array1.length === array2.length
+    && array1.every((value, index) => value === array2[index]);
+};
+
+const assertEqual = function(actual, expected) {
+  if (eqArray(actual, expected)) {
+    return console.log(`${String.fromCodePoint(9989)} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`${String.fromCodePoint(10060)} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual([1, 2, 3], [1, 2, 3]);
+assertEqual([1, 2, 3], [1, 2, 4]);
+assertEqual([8], [1]);
