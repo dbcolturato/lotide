bc6490f611a6451deff98d2939f35438243852d0
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-09-16 14:14:00 +0000

    Implement middle Lotide Problem

--- middle.js
+++ middle.js
@@ -16,8 +16,8 @@ const assertArraysEqual = function(actual, expected) {
 const middle = function(array) {
   if (array.length === 1 || array.length === 2 || array.length === 0) return [];
   if (array.length % 2 === 0) {
-    return [array[Math.floor(array.length/2) - 1], array[Math.floor(array.length/2)]];
-  } else return [array[Math.floor(array.length/2)]];
+    return [array[Math.floor(array.length / 2) - 1], array[Math.floor(array.length / 2)]];
+  } else return [array[Math.floor(array.length / 2)]];
 };
 
 assertArraysEqual(middle([2]), []);
