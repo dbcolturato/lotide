6a1da0f771f9717a39ac3915e43943f47ed64aba
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-09-19 21:22:09 +0000

    Lotide Refactor Example Head

--- head.js
+++ head.js
@@ -1,22 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`${String.fromCodePoint(9989)} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`${String.fromCodePoint(10060)} Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
-function head(array) {
+const head = function (array) {
   if (array.length === 0) {
     return undefined;
   } else return array[0];
 };
 
-
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([7]), 7);
-assertEqual(head([7]), 8);
-assertEqual(head([]), undefined);
+module.exports = head;
