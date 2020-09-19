2226a72a7971f8068ab3d6756620ee175d16d874
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-09-19 22:05:13 +0000

    Refactor assertArraysEqual and middle

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,7 @@
-const eqArray = function(array1, array2) {
-  return Array.isArray(array1)
-    && Array.isArray(array2)
-    && array1.length === array2.length
-    && array1.every((value, index) => value === array2[index]);
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
-  if (eqArray(actual, expected)) {
+  if (eqArrays(actual, expected)) {
     return console.log(`${String.fromCodePoint(9989)} Assertion Passed: ${actual} === ${expected}`);
   } else {
     return console.log(`${String.fromCodePoint(10060)} Assertion Failed: ${actual} !== ${expected}`);
@@ -14,6 +9,4 @@ const assertArraysEqual = function(actual, expected) {
 };
 module.exports = assertArraysEqual;
 
-// assertEqual([1, 2, 3], [1, 2, 3]);
-// assertEqual([1, 2, 3], [1, 2, 4]);
-// assertEqual([8], [1]);
+
