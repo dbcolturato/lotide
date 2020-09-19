520b665a80340c9e35fa8bb2851cf28c3be4db98
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-09-19 21:53:46 +0000

    Refactor eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,8 @@
-const eqArray = function(array1, array2) {
+const eqArrays = function(array1, array2) {
   return Array.isArray(array1)
     && Array.isArray(array2)
     && array1.length === array2.length
     && array1.every((value, index) => value === array2[index]);
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`${String.fromCodePoint(9989)} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`${String.fromCodePoint(10060)} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(eqArray([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArray([1, 2, 3], [1, 2, 4]), true);
-assertEqual(eqArray([8], [1]), true);
-assertEqual(eqArray([]), false);
+module.exports = eqArrays;
