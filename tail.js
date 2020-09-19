e04d7e0b7fda595c4618a258250245815ee6e4a5
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-09-19 21:39:52 +0000

    Refactor tail

--- tail.js
+++ tail.js
@@ -1,17 +1,17 @@
-const arrayEquals = function(array1, array2) {
-  return Array.isArray(array1)
-    && Array.isArray(array2)
-    && array1.length === array2.length
-    && array1.every((value, index) => value === array2[index]);
-};
+// const arrayEquals = function(array1, array2) {
+//   return Array.isArray(array1)
+//     && Array.isArray(array2)
+//     && array1.length === array2.length
+//     && array1.every((value, index) => value === array2[index]);
+// };
 
-const assertEqual = function(actual, expected) {
-  if (arrayEquals(actual, expected)) {
-    return console.log(`${String.fromCodePoint(9989)} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`${String.fromCodePoint(10060)} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (arrayEquals(actual, expected)) {
+//     return console.log(`${String.fromCodePoint(9989)} Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     return console.log(`${String.fromCodePoint(10060)} Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 const tail = function(array) {
   if (array.length === 0 || array.length === 1) {
@@ -19,19 +19,8 @@ const tail = function(array) {
   } else return array.slice(1, array.length);
 };
 
+module.exports = tail;
 
-/*
-const oneElemArray = ["one"];
-const zeroElemArray = [];
-tail(words);
-console.log(tail(oneElemArray));
-console.log(tail(zeroElemArray));
-assertEqual(words.length, 3);
-*/
-assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-assertEqual(tail([1, 2, 3]), [2, 3]);
-assertEqual(tail([8]), []);
-assertEqual(tail([]), []);
 
 
 
