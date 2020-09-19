42f4dae7d1584da6baf398d66570ce4fc07c3b47
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-09-19 21:57:34 +0000

    Refactor tail

--- tail.js
+++ tail.js
@@ -1,18 +1,3 @@
-// const arrayEquals = function(array1, array2) {
-//   return Array.isArray(array1)
-//     && Array.isArray(array2)
-//     && array1.length === array2.length
-//     && array1.every((value, index) => value === array2[index]);
-// };
-
-// const assertEqual = function(actual, expected) {
-//   if (arrayEquals(actual, expected)) {
-//     return console.log(`${String.fromCodePoint(9989)} Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     return console.log(`${String.fromCodePoint(10060)} Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
 const tail = function(array) {
   if (array.length === 0 || array.length === 1) {
     return [];
