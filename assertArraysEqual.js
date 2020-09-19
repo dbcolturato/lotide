8b926934919865d7cfe0f6dea23806bdfe9e9ee5
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-09-19 21:46:24 +0000

    Refactor assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -5,14 +5,15 @@ const eqArray = function(array1, array2) {
     && array1.every((value, index) => value === array2[index]);
 };
 
-const assertEqual = function(actual, expected) {
+const assertArraysEqual = function(actual, expected) {
   if (eqArray(actual, expected)) {
     return console.log(`${String.fromCodePoint(9989)} Assertion Passed: ${actual} === ${expected}`);
   } else {
     return console.log(`${String.fromCodePoint(10060)} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+module.exports = assertArraysEqual;
 
-assertEqual([1, 2, 3], [1, 2, 3]);
-assertEqual([1, 2, 3], [1, 2, 4]);
-assertEqual([8], [1]);
+// assertEqual([1, 2, 3], [1, 2, 3]);
+// assertEqual([1, 2, 3], [1, 2, 4]);
+// assertEqual([8], [1]);
