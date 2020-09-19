e54824400d085238cf08a4012d799d3de6d76cbb
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-09-19 22:28:26 +0000

    Incorporate Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,18 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
+const assert = require('chai').assert;
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([7]), 7);
-assertEqual(head([7]), 8);
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello']), 'Hello'); 
+  });
+  it("returns undefined for []", () => {
+    assert.isUndefined(head([]), undefined);
+  });
+});
